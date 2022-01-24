534c58bbdcdb8087d1c7741b94c58fd5c06441dc
Nathan COTO <coto.nathan71@gmail.com>
2022-01-24 09:46:39 +0100

    Splash Screen init (bugs to fix)

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
+import SplashScreen from 'react-native-splash-screen'
+import React, { useEffect } from 'react'
 
 export default function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
+  
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {Platform.OS === 'ios' && <StatusBar barStyle="light-content" />}
     </View>
   );
 }
